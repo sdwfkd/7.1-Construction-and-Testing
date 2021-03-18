19b959db4f0c66ac669690a07de7d215b75355a7
sdwfkd <77748944+sdwfkd@users.noreply.github.com>
2021-03-17 20:56:19 -0500

    Update check_password_test.py

--- check_password_test.py
+++ check_password_test.py
@@ -2,7 +2,7 @@ import pytest
 import System
 
 #Tests if the program can handle a correct password
-def test_login(grading_system):
+def test_check_password(grading_system):
     username = 'akend3'
     password =  '123454321'
     grading_system.check_password(username,password)
