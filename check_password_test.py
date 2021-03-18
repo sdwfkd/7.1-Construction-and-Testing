112ef7f0a1610b2ec6ddf0f62072c87923aa15ca
sdwfkd <77748944+sdwfkd@users.noreply.github.com>
2021-03-17 20:34:39 -0500

    Update check_password_test.py

--- check_password_test.py
+++ check_password_test.py
@@ -6,3 +6,10 @@ def test_login(grading_system):
     username = 'akend3'
     password =  '123454321'
     grading_system.check_password(username,password)
+    
+
+@pytest.fixture
+def grading_system():
+    gradingSystem = System.System()
+    gradingSystem.load_data()
+    return gradingSystem
