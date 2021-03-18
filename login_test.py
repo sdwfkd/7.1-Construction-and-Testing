6dbc245d3450f754571f02fb019515ea28c87b62
sdwfkd <77748944+sdwfkd@users.noreply.github.com>
2021-03-17 20:31:44 -0500

    Update login_test.py

--- login_test.py
+++ login_test.py
@@ -6,10 +6,3 @@ def test_login(grading_system):
     username = 'akend3'
     password =  '123454321'
     grading_system.login(username,password)
-
-
-@pytest.fixture
-def grading_system():
-    gradingSystem = System.System()
-    gradingSystem.load_data()
-    return gradingSystem
